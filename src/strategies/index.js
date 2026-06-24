f3e18a07b8c86834968f21a23df77d38e21968d2
s1vann <140653998+sivvv0@users.noreply.github.com>
2026-06-24 23:50:22 +0300

    Create index.js

---
+++ src/strategies/index.js
@@ -0,0 +1,11 @@
+const { FlatTax } = require('./FlatTax');
+const { PercentageTax } = require('./PercentageTax');
+const { ProgressiveTax } = require('./ProgressiveTax');
+const { TieredTax } = require('./TieredTax');
+
+module.exports = {
+  FlatTax,
+  PercentageTax,
+  ProgressiveTax,
+  TieredTax
+};
